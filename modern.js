819e39508c74821ffb2b6f92e705516b967f27c0
gt tom <gadhadara918@gmail.com>
2026-08-09 13:13:14 +0530

    Scope

--- modern.js
+++ modern.js
@@ -1,4 +1,5 @@
-const counter = () => {
+// CLOSURES
+/*const counter = () => {
   let count = 0;
   return () => {
     count++;
@@ -9,3 +10,18 @@ const counter = () => {
 const increment = counter();
 console.log(increment()); // 1
 console.log(increment()); // 2
+*/
+// SCOPE
+let global = "Global";
+
+function test() {
+    let local = "Local";
+
+    console.log(global); // ✅
+    console.log(local);  // ✅
+}
+
+// test();
+
+console.log(global); // ✅
+// console.log(local); ❌

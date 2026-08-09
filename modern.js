511964962c5fad80fedc4efb6ebb8432d0c64ce7
gt tom <gadhadara918@gmail.com>
2026-08-09 13:20:13 +0530

    Hosting

--- modern.js
+++ modern.js
@@ -12,16 +12,23 @@ console.log(increment()); // 1
 console.log(increment()); // 2
 */
 // SCOPE
-let global = "Global";
+// let global = "Global";
 
-function test() {
-    let local = "Local";
+// function test() {
+//   let local = "Local";
 
-    console.log(global); // ✅
-    console.log(local);  // ✅
-}
+//   console.log(global);
+//   console.log(local);
+// }
 
 // test();
 
-console.log(global); // ✅
-// console.log(local); ❌
+// console.log(global);
+// console.log(test());
+
+// hosting
+
+console.log(name); // undefined
+console.log(age); // ReferenceError: age is not defined
+var name = "tom";
+var age = 22;

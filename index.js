0d62665591a2f721db7e729a59e124a745a18142
gt tom <gadhadara918@gmail.com>
2026-08-09 10:08:35 +0530

    add data types examples to index.js

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ var a = 10; // var is function-scoped
 let b = 20; // let is block-scoped
 const c = 30; // const is block-scoped and immutable
 console.log(a+b+c);
+
+// Data types
+let name = "Tom";     // String
+let age = 22;            // Number
+let isActive = true;     // Boolean
+let data = null;        // Null
+let value;              // Undefined
+let user = { name };    // Object
+let skills = ["JS", "Node"]; // Array
